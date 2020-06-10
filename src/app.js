9fe766c3fbdc543d1a3b10893fb01322f9c28f13
MiPersona <bgva2005@yahoo.com>
2020-06-10 09:59:04 -0500

    0002_ConDebug

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
 console.log("Hola Mundo");
+
+a = document.getElementById("contenedor");
+for (let index = 0; index < 5; index++) {
+  if (index == 0) {
+    a.innerHTML = index;
+  } else {
+    a.innerHTML = a.innerHTML + "," + index;
+  }
+}
