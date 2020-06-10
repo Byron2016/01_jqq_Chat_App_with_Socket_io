ffdf0611ae814d4f2f810653063207954a0e9068
MiPersona <bgva2005@yahoo.com>
2020-06-10 12:37:12 -0500

    0006_Incorporacion mensages con js

--- src/app.js
+++ src/app.js
@@ -9,5 +9,73 @@ for (let index = 0; index < 5; index++) {
   }
 }
 */
+const messageTypes = { LEFT: 'left', RIGHT: 'right', LOGIN: 'login' };
+
+//chat stuff
+const chatWindow = document.getElementById('chat');
+const messagesList = document.getElementById('messagesList');
+const messageInput = document.getElementById('messageInput');
+const sendBtn = document.getElementById('sendBtn');
+
+//login Stuff
+let username = '';
+const userNameInput = document.getElementById('userNameInput');
+const loginBtn = document.getElementById('loginBtn');
+const loginWindow = document.getElementById('login');
+
+const messages = [
+  {
+    author: 'Byron',
+    date: '11/11/2011',
+    content: 'cool message',
+    type: messageTypes.RIGHT
+  },
+  {
+    author: 'Sofi',
+    date: '11/11/2011',
+    content: 'No tiene contenido',
+    type: messageTypes.LEFT
+  },
+  {
+    author: 'login',
+    date: '11/11/2011',
+    content: 'cool message',
+    type: messageTypes.LOGIN
+  }
+]; //{authoer, date, content, type}
 
 
+//take in message object, and return corresponding message HTML
+const createMessageHTML = (message) => {
+  if (message.type === messageTypes.LOGIN) {
+    return `
+      <p class="secondary-text text-center mb-2">${message.author} has joined the chat...</p>
+    `;
+  }
+
+  return `
+    <div class="message ${
+      message.type === messageTypes.LEFT
+        ? 'message-left'
+        : 'message-right'
+      }">
+      <div id="message-details" class="flex">
+        <p class="message-author">${
+          message.author === messageTypes.RIGHT ? '' : message.author  
+        }</p>
+        <p class="message-date">${message.date}</p>
+      </div>
+      <p class="message-content">${message.content}</p>
+    </div>
+  `
+};
+
+const displayMessages = () => {
+  const messagesHTML = messages
+    .map((message) => createMessageHTML(message))
+    .join('');
+  messagesList.innerHTML = messagesHTML;
+
+};
+
+displayMessages();
